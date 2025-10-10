6a1be0282f0d5fc06ea5d3d24bca0414b94dce6e
Harris Bond <hb625@cam.ac.uk>
2025-10-10 15:20:46 +0100

    Attempting control of Motor4, EDF

--- ArduCopter/mode_test.cpp
+++ ArduCopter/mode_test.cpp
@@ -96,9 +96,9 @@
 void ModeTest::run()
 {
     // 0 = SERVO1 physical pin
-    SRV_Channel* ch = SRV_Channels::srv_channel(1);
+    SRV_Channel* ch = SRV_Channels::srv_channel(3);
     if (ch) {
-        uint16_t pwm_us = 1500 + (int16_t)(sinf((float)AP_HAL::millis() / 1000.0f) * 400.0f); // ±400us example
+        uint16_t pwm_us = 1050 + (int16_t)(sinf((float)AP_HAL::millis() / 1000.0f) * 50.0f); // ±400us example
         ch->set_output_pwm(pwm_us, true);   // force = true to ensure write
     }
 
