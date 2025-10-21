6a488dd6b55873104900b80ae841a9f254a72b05
Harris Bond <hb625@cam.ac.uk>
2025-10-21 15:39:35 +0100

    Changed to 1070us

--- ArduCopter/mode_test.cpp
+++ ArduCopter/mode_test.cpp
@@ -128,10 +128,10 @@ void ModeTest::run()
     uint32_t elapsed_time_ms = AP_HAL::millis() - start_time;
     if (ch) {
         uint16_t pwm_us;
-        if (elapsed_time_ms < 25000 || elapsed_time_ms > 45000){
+        if (elapsed_time_ms < 25000 || elapsed_time_ms > 35000){
             pwm_us = 1000;
         } else {
-            pwm_us = 1100;
+            pwm_us = 1070;
         }
         ch->set_output_pwm(pwm_us, true);   // force = true to ensure write
     }
