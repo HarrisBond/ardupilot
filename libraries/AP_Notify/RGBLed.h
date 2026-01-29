06eb10702dd7f93b25ab0b78b76f8aa00d4387a3
Harris Bond <hb625@cam.ac.uk>
2026-01-29 13:46:07 +0000

    Attempt LED Addressing 4

--- libraries/AP_Notify/RGBLed.h
+++ libraries/AP_Notify/RGBLed.h
@@ -44,7 +44,7 @@ public:
 protected:
     // methods implemented in hardware specific classes
     virtual bool hw_set_rgb(uint8_t red, uint8_t green, uint8_t blue) = 0;
-    virtual bool hw_set_rgb(uint8_t led, uint8_t red, uint8_t green, uint8_t blue) = 0;
+    bool hw_set_rgb(uint8_t led, uint8_t red, uint8_t green, uint8_t blue) = 0;
 
     // set_rgb - set color as a combination of red, green and blue levels from 0 ~ 15
     virtual void _set_rgb(uint8_t red, uint8_t green, uint8_t blue);
