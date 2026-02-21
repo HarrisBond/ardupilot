0873419e3c18dad4f60644cdfd8efb12a4ebff42
Harris Bond <hb625@cam.ac.uk>
2026-02-21 15:59:52 +0000

    attempt controller 20

--- ArduCopter/harris_matrix.h
+++ ArduCopter/harris_matrix.h
@@ -70,3 +70,14 @@ public:
         return result;
     }
 };
+
+operator*(T scalar, const Matrix<T, Rows, Cols>& mat) noexcept
+{
+    Matrix<T, Rows, Cols> result;
+
+    for (std::size_t i = 0; i < Rows; ++i)
+        for (std::size_t j = 0; j < Cols; ++j)
+            result(i,j) = scalar * mat(i,j);
+
+    return result;
+}
